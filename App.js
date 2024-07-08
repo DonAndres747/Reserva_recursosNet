a79376275e046acea47bbbc3e324781c9dd22376
Andres Felipe Giraldo Bustos <andres.giraldo@netlogistik.com>
2024-07-08 11:31:45 -0500

    commit para conpilacion android

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+// import app from '@react-native-firebase/app';
+// import messaging from '@react-native-firebase/messaging';
+
 import MainStack from "./Navigation/MainStack.js";
 
+ 
+
 export default function App() {
-  // <script src="http://localhost:8097"></script>
+ 
+  
+
   return (
-    <MainStack/>
+    <MainStack />
   );
 }
 
