15ead639c86de79acad88cd2c9f0e9e0813beac8
Andres Felipe Giraldo Bustos <andres.giraldo@netlogistik.com>
2024-08-09 23:06:07 -0500

    se modifico vista disponibilidad para listar habilidades goLive

--- src/componentes/Availability/AvailabilityTypSeg.jsx
+++ src/componentes/Availability/AvailabilityTypSeg.jsx
@@ -1,166 +1,89 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { CheckBox } from "react-native-elements";
+import { useTranslation } from "react-i18next";
+
 import theme from "../../theme";
+import '../../helpers/i18n'
+
 
 export default function AvailabilityTypSeg({ onChange }) {
-
-    const [checkboxStates, setCheckboxStates] = useState(Array(6).fill(false));
+    const [checkboxStates, setCheckboxStates] = useState(Array(array.length).fill(false));
+    const { t } = useTranslation();
 
     const handleCheckboxChange = (index) => {
         const updatedStates = [...checkboxStates];
         updatedStates[index] = !updatedStates[index];
         setCheckboxStates(updatedStates);
-        const checkItems = [];
 
+        const checkItems = [];
         updatedStates.forEach((selectedItem, index) => {
             if (selectedItem) {
+                console.log(array[index].id);
                 checkItems.push(array[index].id);
             }
         });
-        onChange(checkItems)
+        onChange(checkItems);
     };
-
 
     return (
         <View style={styles.container}>
-            <View style={styles.checkboxContainer}>
-                <CheckBox
-                    checked={checkboxStates[0]}
-                    size={20}
-                    iconType="material"
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle"
-                    checkedColor={theme.colors.naranjaNet}
-                    unCheckedColor={{ backgroundColor: "#d3d3d3" }}
-                    containerStyle={styles.checkbox}
-                    title="Bilingue"
-                    textStyle={styles.label}
-                    onPress={() => handleCheckboxChange(0)}
-                />
-                <CheckBox
-                    checked={checkboxStates[1]}
-                    size={20}
-                    iconType="material"
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle"
-                    checkedColor={theme.colors.naranjaNet}
-                    unCheckedColor={{ backgroundColor: "#d3d3d3" }}
-                    containerStyle={styles.checkbox}
-                    title="Visa"
-                    textStyle={styles.label}
-                    onPress={() => handleCheckboxChange(1)}
-                />
-                <CheckBox
-                    checked={checkboxStates[2]}
-                    size={20}
-                    iconType="material"
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle"
-                    checkedColor={theme.colors.naranjaNet}
-                    unCheckedColor={{ backgroundColor: "#d3d3d3" }}
-                    containerStyle={styles.checkbox}
-                    title="Visa"
-                    textStyle={styles.label}
-                    onPress={() => handleCheckboxChange(2)}
-                />
-            </View>
-            <View style={styles.checkboxContainer}>
-                <CheckBox
-                    checked={checkboxStates[3]}
-                    size={20}
-                    iconType="material"
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle"
-                    checkedColor={theme.colors.naranjaNet}
-                    unCheckedColor={{ backgroundColor: "#d3d3d3" }}
-                    containerStyle={styles.checkbox}
-                    title="Configuracion"
-                    textStyle={styles.label}
-                    onPress={() => handleCheckboxChange(3)}
-                />
-                <CheckBox
-                    checked={checkboxStates[4]}
-                    size={20}
-                    iconType="material"
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle"
-                    checkedColor={theme.colors.naranjaNet}
-                    unCheckedColor={{ backgroundColor: "#d3d3d3" }}
-                    containerStyle={styles.checkbox}
-                    title="Desarrollo"
-                    textStyle={styles.label}
-                    onPress={() => handleCheckboxChange(4)}
-                />
-                <CheckBox
-                    checked={checkboxStates[5]}
-                    size={20}
-                    iconType="material"
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle"
-                    checkedColor={theme.colors.naranjaNet}
-                    unCheckedColor={{ backgroundColor: "#d3d3d3" }}
-                    containerStyle={styles.checkbox}
-                    title="Integraciones"
-                    textStyle={styles.label}
-                    onPress={() => handleCheckboxChange(5)}
-                />
-            </View>
+            <ScrollView
+                contentContainerStyle={styles.checkboxContainer}
+                showsVerticalScrollIndicator={false}
+            >
+                {array.map((item, index) => (
+                    <CheckBox
+                        key={item.id}
+                        checked={checkboxStates[index]}
+                        size={20}
+                        iconType="material"
+                        checkedIcon="check-circle"
+                        uncheckedIcon="circle"
+                        checkedColor={theme.colors.naranjaNet}
+                        unCheckedColor={{ backgroundColor: "#d3d3d3" }}
+                        containerStyle={styles.checkbox}
+                        title={t(`${item.name}`)}
+                        textStyle={styles.label}
+                        onPress={() => handleCheckboxChange(index)}
+                    />
+                ))}
+            </ScrollView>
         </View>
     );
 }
 
 const array = [
-    {
-        name: "Bilingue",
-        id: "BIL"
-    },
-    {
-        name: "Visa",
-        id: "VISA"
-    },
-    {
-        name: "Visa",
-        id: "VISA"
-    },
-    {
-        name: "Configuracion",
-        id: "CONF"
-    },
-    {
-        name: "Desarrollo",
-        id: "DES"
-    },
-    {
-        name: "Integraciones",
-        id: "INTE"
-    },
+    { name: "availability.OTHER.PPT", id: "PPT" },
+    { name: "availability.OTHER.VISAEEUU", id: "VISAEEUU" },
+    { name: "availability.OTHER.VISACAN", id: "VISACAN" },
+    { name: "availability.language.ENG", id: "ENG" },
+    { name: "availability.language.POR", id: "POR" },
+    { name: "availability.language.FRA", id: "FRA" },
+    { name: "availability.language.SPA", id: "SPA" },
+    { name: "availability.servtyp.CONF", id: "CONF" },
+    { name: "availability.servtyp.CONS", id: "CONS" },
+    { name: "availability.servtyp.DEV", id: "DEV" },
+    { name: "availability.servtyp.TECH", id: "TECH" },
 ];
-
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
         marginVertical: 20,
-        justifyContent: "flex-start"
+        height: 150,
+        padding: 0,
     },
     checkboxContainer: {
-        flexDirection: "column",
-        width: "50%"
+        flexDirection: "row",
+        flexWrap: "wrap",
     },
     label: {
         marginLeft: 8,
         fontSize: Platform.OS === 'ios' ? 15 : 17
     },
     checkbox: {
-        margin: 0,
+        width: "40%",
         backgroundColor: '#f2f2f2',
         borderWidth: 0,
-    },
-    checkboxIcon: {
-        width: 14,
-        height: 14,
-        backgroundColor: "#d3d3d3",
-        borderRadius: 10,
     },
 });
--- src/helpers/rolesConfig.js
+++ src/helpers/rolesConfig.js
@@ -4,10 +4,12 @@ export const rolesConfig = {
   1: ["Booking", "Components", "netRewards", "Planificacion"],
   2: ["Disponibilidad", "Booking", "Components", "netRewards", "Planificacion",],
   3: ["Disponibilidad", "Booking", "Components", "netRewards", "Planificacion", "Request"],
+  0: ["Disponibilidad", "Booking", "Components", "netRewards", "Planificacion", "Request"],
 };
 
 export const rolesDefinition = {
   1: "cliente",
   2: "Recurso",
   3: "Gerente",
+  0: "admin"
 };
